c700ee0ea9b7565e803819aed2d6819966c0fb14
lms8147 <lms8147@gmail.com>
2018-07-29 19:42:50 +0900

    define interface (  * one resource - one object  * one resource - at most 4 method (GET, POST, UPDATE, DELETE)  * Use Generic and Value Object  )

---
+++ app/src/main/java/com/tou4u/interfacerestapi/restapi/RestApiRequester.java
@@ -0,0 +1,17 @@
+package com.tou4u.interfacerestapi.restapi;
+
+/**
+ * one resource - one object
+ * one resource - at most 4 method(GET, POST, UPDATE, DELETE)
+ */
+public interface RestApiRequester<GetReqVO, PostReqVO, UpdateReqVO, DeleteReqVO> {
+
+    public <GetResVO> GetResVO excuteGet(GetReqVO requestVO, Class<GetResVO> responseType);
+
+    public <PostResVO> PostResVO excutePost(PostReqVO requestVO, Class<PostResVO> responseType);
+
+    public <UpdateResVO> UpdateResVO excuteUpdate(UpdateReqVO requestVO, Class<UpdateResVO> responseType);
+
+    public <DeleteResVO> DeleteResVO excuteDelete(DeleteReqVO requestVO, Class<DeleteResVO> responseType);
+    
+}
