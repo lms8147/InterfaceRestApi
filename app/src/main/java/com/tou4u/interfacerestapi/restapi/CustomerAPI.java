4f85a87ccde61b056f093bbe81fa9e1105c5c1e1
lms <lms8147@gmail.com>
2018-07-30 00:38:57 +0900

    solve below problem{  * url mapping procedure duplication  * each method body is similar to the other } -> change to annotation and reflection

--- app/src/main/java/com/tou4u/interfacerestapi/restapi/CustomerAPI.java
+++ app/src/main/java/com/tou4u/interfacerestapi/restapi/CustomerAPI.java
@@ -2,65 +2,61 @@ package com.tou4u.interfacerestapi.restapi;
 
 import com.tou4u.interfacerestapi.restapi.base.RestApiBaseRequester;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * http://www.example.com/customers/{cid} rest api
- *
  */
-public class CustomerAPI extends RestApiBaseRequester<CustomerAPI.GetReqVO, CustomerAPI.PostReqVO, CustomerAPI.UpdateReqVO, CustomerAPI.DeleteReqVO> {
-
-    @Override
-    public <GetResVO> GetResVO excuteGet(GetReqVO requestVO, Class<GetResVO> responseType) {
-        String specificURL = mBaseURL.replaceFirst("uid","" + requestVO.id);
-        Map<String, Object> params = new HashMap<>();
-        params.put("age",requestVO.age);
-        return requestVOWithGSON(specificURL, params, responseType);
-    }
-
-    @Override
-    public <PostResVO> PostResVO excutePost(PostReqVO requestVO, Class<PostResVO> responseType) {
-        String specificURL = mBaseURL.replaceFirst("uid","" + requestVO.id);
-        Map<String, Object> params = new HashMap<>();
-        params.put("age",requestVO.age);
-        return requestVOWithGSON(specificURL, params, responseType);
-    }
-
-    @Override
-    public <UpdateResVO> UpdateResVO excuteUpdate(UpdateReqVO requestVO, Class<UpdateResVO> responseType) {
-        String specificURL = mBaseURL.replaceFirst("uid","" + requestVO.id);
-        Map<String, Object> params = new HashMap<>();
-        params.put("age",requestVO.age);
-        return requestVOWithGSON(specificURL, params, responseType);
-    }
-
-    @Override
-    public <DeleteResVO> DeleteResVO excuteDelete(DeleteReqVO requestVO, Class<DeleteResVO> responseType) {
-        String specificURL = mBaseURL.replaceFirst("uid","" + requestVO.id);
-        return requestVOWithGSON(specificURL, null, responseType);
-    }
+public class CustomerAPI extends RestApiBaseRequester {
 
     public CustomerAPI() {
         super(" http://www.example.com/customers/{cid}");
     }
 
-    public static class GetReqVO{
-        public int id;
-        public int age;
+    @RequestVO(methodType = METHOD_GET)
+    public static class GetReqVO {
+        @Argument
+        public final int cid;
+        @Parameter
+        public final int age;
+
+        public GetReqVO(int cid, int age) {
+            this.cid = cid;
+            this.age = age;
+        }
     }
 
-    public static class PostReqVO{
-        public int id;
-        public int age;
+    @RequestVO(methodType = METHOD_POST)
+    public static class PostReqVO {
+        @Argument
+        public final int cid;
+        @Parameter
+        public final int age;
+
+        public PostReqVO(int cid, int age) {
+            this.cid = cid;
+            this.age = age;
+        }
     }
 
-    public static class UpdateReqVO{
-        public int id;
-        public int age;
+    @RequestVO(methodType = METHOD_UPDATE)
+    public static class UpdateReqVO {
+        @Argument
+        public final int cid;
+        @Parameter
+        public final int age;
+
+        public UpdateReqVO(int cid, int age) {
+            this.cid = cid;
+            this.age = age;
+        }
     }
 
-    public static class DeleteReqVO{
-        public int id;
+    @RequestVO(methodType = METHOD_DELETE)
+    public static class DeleteReqVO {
+        @Argument
+        public final int cid;
+
+        public DeleteReqVO(int cid) {
+            this.cid = cid;
+        }
     }
 }
--- app/src/main/java/com/tou4u/interfacerestapi/restapi/base/RestApiBaseRequester.java
+++ app/src/main/java/com/tou4u/interfacerestapi/restapi/base/RestApiBaseRequester.java
@@ -3,10 +3,17 @@ package com.tou4u.interfacerestapi.restapi.base;
 import com.google.gson.Gson;
 import com.tou4u.interfacerestapi.httpconnection.cast.HttpStringRequester;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
-public abstract class RestApiBaseRequester<GetReqVO, PostReqVO, UpdateReqVO, DeleteReqVO> implements RestApiRequester<GetReqVO, PostReqVO, UpdateReqVO, DeleteReqVO> {
+public abstract class RestApiBaseRequester implements RestApiRequester {
 
     private final HttpStringRequester mHttpStringRequester;
     private final Map<String, String> mHttpHeader;
@@ -22,9 +29,81 @@ public abstract class RestApiBaseRequester<GetReqVO, PostReqVO, UpdateReqVO, Del
 
     }
 
-    public <ReqVO, ResVO> ResVO requestVOWithGSON(String specificURL, Map<String, Object> params, Class<ResVO> responseType) {
-        String json = mHttpStringRequester.requestGetData(specificURL, mHttpHeader, params);
-        Gson gson = new Gson();
-        return gson.fromJson(json, responseType);
+    public <ReqVO, ResVO> ResVO excuteMethod(@Method int method, ReqVO requestVO, Class<ResVO> responseType) {
+
+        Class reqeustType = requestVO.getClass();
+
+        RequestVO voType = (RequestVO) reqeustType.getAnnotation(RequestVO.class);
+        if(voType == null){
+            return null;
+        }else{
+            if(voType.methodType() != method){
+                return null;
+            }
+        }
+
+        Map<String, Object> params = new HashMap<>();
+        String specificURL = mBaseURL;
+        try {
+            for (Field field : reqeustType.getFields()) {
+                Argument typeArgument = field.getAnnotation(Argument.class);
+                if (typeArgument != null) {
+                    String name = field.getName();
+                    Object value = null;
+
+                    value = field.get(requestVO);
+
+                    specificURL = specificURL.replace(name, value.toString());
+                } else {
+                    Parameter typeParameter = field.getAnnotation(Parameter.class);
+                    if (typeParameter != null) {
+                        String name = field.getName();
+                        Object value = field.get(requestVO);
+                        params.put(name, value);
+                    }
+                }
+            }
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        if (params.size() == 0)
+            params = null;
+
+        String json = null;
+        switch (method) {
+            case METHOD_DELETE:
+                json = mHttpStringRequester.requestDeleteData(specificURL, mHttpHeader, null, params);
+                break;
+            case METHOD_GET:
+                json = mHttpStringRequester.requestGetData(specificURL, mHttpHeader, params);
+                break;
+            case METHOD_POST:
+                json = mHttpStringRequester.requestPostData(specificURL, mHttpHeader, null, params);
+                break;
+            case METHOD_UPDATE:
+                json = mHttpStringRequester.requestUpdateData(specificURL, mHttpHeader, null, params);
+                break;
+        }
+        if (json != null) {
+            Gson gson = new Gson();
+            return gson.fromJson(json, responseType);
+        } else {
+            return null;
+        }
+
+    }
+
+    @Documented
+    @Target(ElementType.FIELD)
+    @Inherited
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface Argument {
+    }
+
+    @Documented
+    @Target(ElementType.FIELD)
+    @Inherited
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface Parameter {
     }
 }
